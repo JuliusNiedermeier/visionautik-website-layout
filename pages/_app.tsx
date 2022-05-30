9e3c6f905e9fd16e11b1bfccea6c3528cf9c51f8
Julius Niedermeier <julius.niedermeier@outlook.de>
2022-05-30 13:53:19 +0200

    Setup theming with StyledComponents

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { AppProps } from "next/app";
+import { ThemeProvider } from "styled-components";
+import theme from "../theme";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
