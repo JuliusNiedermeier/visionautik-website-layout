0c54a0f57a236f1b4609a74a2cd74b26f2dd3b6e
Julius Niedermeier <julius.niedermeier@outlook.de>
2022-06-03 00:40:18 +0200

    Added Prismic features
    - Prismic Provider
    - Prismic Preview
    - Sice Machine
    - Link Resolver

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,20 @@ import { AppProps } from "next/app";
 import { ThemeProvider } from "styled-components";
 import theme from "../theme";
 
+import { PrismicProvider } from "@prismicio/react";
+import { PrismicPreview } from "@prismicio/next";
+import { linkResolver, repositoryName } from "../prismicio";
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <PrismicProvider linkResolver={linkResolver}>
+        <PrismicPreview repositoryName={repositoryName}>
+          <ThemeProvider theme={theme}>
+            <Component {...pageProps} />
+          </ThemeProvider>
+        </PrismicPreview>
+      </PrismicProvider>
     </>
   );
 }
---
+++ scripts/TODO.md
@@ -0,0 +1,4 @@
+### ToDo
+
+1. Write a script that generates types for each prismic slice defined in the slices folder
+2. Write a script that generates types for each prismic custom type in the customtypes folder
